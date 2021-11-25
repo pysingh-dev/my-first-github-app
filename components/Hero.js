4ed6e179ce9d0c6962b78986fe0b649f38dd76cf
bhattjaldhi <bhattjaldhi27@gmail.com>
2021-11-25 22:23:53 +0530

    Update error page

--- components/Hero.js
+++ components/Hero.js
@@ -3,12 +3,14 @@ import React from "react";
 export default function Hero() {
   return (
     <div className="">
-      <img
-        src="./cover.jpg"
-        alt="under maintainance"
-        className="error-image"
-        id="coverImage"
-      />
+      <a href="https://twitter.com/eduecosystem1" target="_blank">
+        <img
+          src="./cover.jpg"
+          alt="under maintainance"
+          className="error-image"
+          id="coverImage"
+        />
+      </a>
     </div>
   );
 }
